01e8326847f55b33c96e945ac93a0297e0a4c73e
gabs2678 <gabrieldelrio3@gmail.com>
2023-08-02 16:37:47 -0400

    commit3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { AudioClip } from './type'
 import Drum from './Drum';
--- src/Drum.tsx
+++ src/Drum.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { AudioClip } from './type'
 import './App.css'
 
